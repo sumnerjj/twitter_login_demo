b069520c8c134b2094e31255d07f1c8aa3e41ba1
Justin Sumner <jsumner@hearsaycorp.com>
2017-09-28 15:10:12 -0700

    asd

---
+++ Twitter_login_demo/models/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  Twitter_login_demo
+//
+//  Created by Justin Sumner on 9/28/17.
+//  Copyright © 2017 Justin Sumner. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
---
+++ Twitter_login_demo/models/User.swift
@@ -0,0 +1,29 @@
+//
+//  User.swift
+//  Twitter_login_demo
+//
+//  Created by Justin Sumner on 9/28/17.
+//  Copyright © 2017 Justin Sumner. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var name : String?
+    var screenName : String?
+    var profileUrl : URL?
+    var tagline: String?
+    
+    init(dictionary: NSDictionary) {
+        name = dictionary["name"] as? String
+        screenName = dictionary["screen_name"] as? String
+        let profileUrlString = dictionary["profile_image_url_https"] as? String
+        if let profileUrlString = profileUrlString {
+            profileUrl = URL(fileURLWithPath: profileUrlString)
+        }
+        tagline = dictionary["description"] as? String
+        
+    }
+
+}
