31f6eb3d2549cc39dc4671aea86fe3069379665f
Justin Sumner <jsumner@hearsaycorp.com>
2017-09-28 15:39:57 -0700

    asd

--- Twitter_login_demo/models/Tweet.swift
+++ Twitter_login_demo/models/Tweet.swift
@@ -9,5 +9,35 @@
 import UIKit
 
 class Tweet: NSObject {
+    
+    var text : String?
+    var timeStamp : Date?
+    var retweetCount : Int = 0
+    var favoritesCount : Int = 0
+    
+    init(dictionary: NSDictionary) {
+        text = dictionary["text"] as? String
+        retweetCount = (dictionary["retweet_count"] as? Int) ?? 0
+        favoritesCount = (dictionary["favourites_count"] as? Int) ?? 0
 
+        let timeStampString = dictionary["created_at"] as? String
+        
+        if let timeStampString = timeStampString {
+            let formatter = DateFormatter()
+            formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+            timeStamp = formatter.date(from: timeStampString)
+        }
+        
+    }
+    
+    class func tweetsWithArray(dictionaries: [NSDictionary]) -> [Tweet] {
+        var tweets = [Tweet]()
+        
+        for dictionary in dictionaries {
+            let tweet = Tweet(dictionary: dictionary)
+            tweets.append(tweet)
+        }
+        return tweets
+    }
+    
 }
