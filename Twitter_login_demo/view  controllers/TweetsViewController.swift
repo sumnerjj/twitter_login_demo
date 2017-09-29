bc8664a2ab1aee9110a2509beb4afda0b1f72021
Justin S <JustinJSumner@gmail.com>
2017-09-28 21:30:25 -0700

    Set tweets

---
+++ Twitter_login_demo/view  controllers/TweetsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TweetsViewController.swift
+//  Twitter_login_demo
+//
+//  Created by Justin Sumner on 9/28/17.
+//  Copyright © 2017 Justin Sumner. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+    
+    var tweets: [Tweet]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        TwitterClient.sharedInstance?.homeTimeline(success: { (tweets: [Tweet]) in
+            self.tweets = tweets
+            for tweet in tweets {
+                print("in the tweets view controller tweet: \(tweet.text)")
+            }
+        }, failure: { (error: Error) in
+            print(error.localizedDescription)
+        })
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
